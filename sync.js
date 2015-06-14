0abb075de32d4b505fd285fb00987d2b277c6bd8
tfarrell-vz <farrell@abiresearch.com>
2015-06-13 22:27:34 -0400

    Read a file, split the lines, count how many there are (adjusting for trailing newline).

--- sync.js
+++ sync.js
@@ -1,2 +1,10 @@
+var fs = require('fs');
 var file = process.argv[2];
-console.log(file);
+
+var buf = fs.readFileSync(file).toString();
+
+var num_lines = buf.split('\n').length-1
+
+console.log(num_lines);
+
+
