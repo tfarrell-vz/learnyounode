83b7ce16451ded2c0a7ad17fbe4be88e942b3e8a
tfarrell-vz <farrell@abiresearch.com>
2015-08-05 17:16:13 -0400

    Handle the problem in a modular fashion.

--- make_it_modular/modreader.js
+++ make_it_modular/modreader.js
@@ -1,4 +1,18 @@
 var mod = require('./modular.js');
 
-console.log(mod(1,2, function(a, b) {return a+b}));
+var dir = process.argv[2];
+var ext = process.argv[3];
+
+function printFilteredDirs(err, data) {
+  if (err)
+    return console.log("Error, Mingus.");
+
+  for (var i=0; i < data.length; i++) {
+    console.log(data[i]);
+  };
+};
+
+mod(dir, ext, printFilteredDirs);
+
+
 
--- make_it_modular/modular.js
+++ make_it_modular/modular.js
@@ -1,4 +1,19 @@
-module.exports = function(a, b, func) {
-  return func(a,b);
-}
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(dir, ext, callback) {
+  var dirs = fs.readdir(dir, function(err, data) {
+    var out = [];
+    if (err)
+      return callback(err)
+
+    for (var i=0; i < data.length; i++) {
+      if (path.extname(data[i]).slice(1) == ext) {
+        out.push(data[i]);
+      }
+    };
+
+    callback(null, out);
+  });
+};
 
