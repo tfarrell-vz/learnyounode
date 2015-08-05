27467d1ea5593275dec2db3dd3eead35644292d5
tfarrell-vz <farrell@abiresearch.com>
2015-08-05 18:11:24 -0400

    Let's see some headers

--- http_client/http_client.js
+++ http_client/http_client.js
@@ -1,7 +1,8 @@
 var http = require('http');
 
 http.get('www.bassnectar.net/tour', function(res) {
-  console.log("Got response: " + res.statusCode);
+  console.log("STATUS: " + res.statusCode);
+  console.log("HEADERS: " + JSON.stringify(res.headers));
 }).on('error', function(e) {
   console.log("Got error: " + e.message);
 });
