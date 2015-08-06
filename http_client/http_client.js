0303e691a5a86b544582f8156a4d1ea93ee26a66
tfarrell-vz <farrell@abiresearch.com>
2015-08-06 10:15:14 -0400

    Get the tour page appropriately.

--- http_client/http_client.js
+++ http_client/http_client.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-http.get('www.bassnectar.net/tour', function(res) {
+http.get('http://www.bassnectar.net/tour/', function(res) {
   console.log("STATUS: " + res.statusCode);
   console.log("HEADERS: " + JSON.stringify(res.headers));
   res.setEncoding('utf8');
