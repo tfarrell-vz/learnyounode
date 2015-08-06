2d62ade6c9a6365b9e4f578c87c4baa14668ccf9
tfarrell-vz <farrell@abiresearch.com>
2015-08-06 09:29:27 -0400

    Get the body

--- http_client/http_client.js
+++ http_client/http_client.js
@@ -3,6 +3,10 @@ var http = require('http');
 http.get('www.bassnectar.net/tour', function(res) {
   console.log("STATUS: " + res.statusCode);
   console.log("HEADERS: " + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function(chunk) {
+    console.log('BODY: ' + chunk);
+  });
 }).on('error', function(e) {
   console.log("Got error: " + e.message);
 });
